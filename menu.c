b7bf2b71ac00f050338d3e9a76cbd8dd4bdff38f
Julian Torres <juliannogueira@yahoo.com>
2022-01-22 09:27:20 -1000

    Update the main menu and add the submenu.

--- menu.c
+++ menu.c
@@ -5,26 +5,72 @@
  * 
  * The user is prompted to enter a menu option.
  * 
- * If the user enters an invalid option, the user is notified.
- * 
- * If the user enters a valid option, the user will get the respective data or be
- * prompted for additional information if necessary.
+ * If the user enters an invalid option, the user is notified. Otherwise, the
+ * user will be taken to the submenu or the program will be terminated.
  */
-void runMenu(struct Movie *head) {
-    int runProgram = 1;
+void runMenu() {
+    int isRunning = 1;
     const int SIZE = 255;
     char buffer[SIZE];
-    char choice;
-    char *header =         "------------------------ Movies: Main Menu -------------------------\n";
+    char option = '\0';
+    char *header =         "-------------------- Movies by Year: Main Menu ---------------------\n";
     char *headerLine =     "--------------------------------------------------------------------\n";
-    char *option1 =        "1) Show movies released in a specified year\n";
-    char *option2 =        "2) Show highest rated movie for each year\n";
-    char *option3 =        "3) Show the title and year of movies in a specified language\n";
-    char *option4 =        "4) Quit the program\n";
+    char *option1 =        "1) Select a file to process\n";
+    char *option2 =        "2) Quit the program\n";
     char *prompt =         "\nEnter a valid menu option: ";
-    char *invalidChoice =  "\nAn invalid menu option was entered.\n";
+    char *invalidOption =  "\nAn invalid menu option was entered.\n";
 
-    while (runProgram) {
+    while (isRunning) {
+        printf("\n\n%s", headerLine);
+        printf("%s", header);
+        printf("%s", headerLine);
+        printf("%s", option1);
+        printf("%s", option2);
+        printf("%s", prompt);
+
+        fgets(buffer, SIZE, stdin);
+        option = buffer[0];
+
+        switch (option) {
+            case '1':
+                runSubmenu();
+                break;
+            case '2':
+                isRunning = 0;
+                printf("\n\n");
+                break;
+            default:
+                printf("%s", invalidOption);
+                break;
+        }
+    }
+}
+
+/*
+ * Display the submenu to the user.
+ * 
+ * The user is prompted to enter a menu option.
+ * 
+ * If the user enters an invalid option, the user is notified. Otherwise, the
+ * user chooses to process the largest, smallest, or specified file.
+ * 
+ * The user also has the choice to return to the main menu.
+ */
+void runSubmenu() {
+    int isRunning = 1;
+    const int SIZE = 255;
+    char buffer[SIZE];
+    char option = '\0';
+    char *header =         "------------------ Movies by Year: Process a File ------------------\n";
+    char *headerLine =     "--------------------------------------------------------------------\n";
+    char *option1 =        "1) Process the largest file\n";
+    char *option2 =        "2) Process the smallest file\n";
+    char *option3 =        "3) Process a specified file\n";
+    char *option4 =        "4) Return to the main menu\n";
+    char *prompt =         "\nEnter a valid menu option: ";
+    char *invalidOption =  "\nAn invalid menu option was entered.\n";
+
+    while (isRunning) {
         printf("\n\n%s", headerLine);
         printf("%s", header);
         printf("%s", headerLine);
@@ -35,24 +81,23 @@ void runMenu(struct Movie *head) {
         printf("%s", prompt);
 
         fgets(buffer, SIZE, stdin);
-        choice = buffer[0];
+        option = buffer[0];
 
-        switch (choice) {
+        switch (option) {
             case '1':
-                getMoviesByYear(head);
+                printf("\nCase 1.\n");
                 break;
             case '2':
-                getHighestRatedMoviePerYear(head);
+                printf("\nCase 2.\n");
                 break;
             case '3':
-                getMoviesByLanguage(head);
+                printf("\nCase 3.\n");
                 break;
             case '4':
-                runProgram = 0;
-                printf("\n\n");
+                isRunning = 0;
                 break;
             default:
-                printf("%s", invalidChoice);
+                printf("%s", invalidOption);
                 break;
         }
     }

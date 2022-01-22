0788b9130bf82c3f31481a1dd3cb3523d851414d
Julian Torres <juliannogueira@yahoo.com>
2022-01-22 11:28:02 -1000

    Add parsing functions.

--- util.h
+++ util.h
@@ -1,7 +1,12 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 /*
  * The util file contains helper functions to be used by main, movie, and menu.
@@ -28,5 +33,13 @@ int isValidDigit(char ch);
 void clearBuffer(char *buffer, int size);
 
 int stringLength(char *string);
+
+void copyString(char *source, char *destination);
+
+int parseString(char *pattern, char *string);
+
+int parseExtension(char *extension, char *filename);
+
+void getParsedFile(char *filename, char *prefix, char *extension, int type);
 
 #endif

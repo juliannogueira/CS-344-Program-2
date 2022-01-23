b1b26a059c5d25c84ce0fe69ff4b1a08aa9dc520
Julian Torres <juliannogueira@yahoo.com>
2022-01-22 16:47:52 -1000

    Align variable name with function definition.

--- processMovieFile.c
+++ processMovieFile.c
@@ -5,12 +5,12 @@
  * Process the largest movie file in the current directory with a prefix of
  * movies_ and an extension of .csv.
  */
-void processMovieFile(int type) {
+void processMovieFile(int getLargestFile) {
     char *prefix = "movies_";
     char *extension = ".csv";
     char *filename = malloc(sizeof(char) * 256);
     *filename = '\0';
-    getParsedFile(filename, prefix, extension, type);
+    getParsedFilename(filename, prefix, extension, getLargestFile);
     if (*filename != '\0') {
         printf("\n%s\n", filename);
     }
